c7d290f47b3c5bd87e3b85d2bc1df4ab103ff2a2
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-10-28 15:03:26 +0100

    Added Button example.

---
+++ examples/02_button.rb
@@ -0,0 +1,17 @@
+require_relative 'window'
+
+class ExampleState < GuiState
+  def setup
+    VerticalPacker.new(container) do |packer|
+      label = Label.new(packer, text: "Label")
+
+      Button.new(packer, text: "Button") do |button|
+        button.subscribe :clicked_left_mouse_button do |sender, x, y|
+          label.text = "Pressed the button!"
+        end
+      end
+    end
+  end
+end
+
+Window.new.show
