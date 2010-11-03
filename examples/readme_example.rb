62c6d33990ec35134ce4437ba095e9cea51add5e
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-11-03 14:56:06 +0000

    Expanded readme.

---
+++ examples/readme_example.rb
@@ -0,0 +1,32 @@
+require_relative "../lib/fidgit"
+
+# Normally, you'd load this from the gem.
+# require 'fidgit'
+
+class MyGame < Chingu::Window
+  def initialize
+    super(640, 480, false)
+  end
+  
+  def setup
+    push_game_state MyGuiState # To use Fidgit, a Fidgit::GuiState must be active.
+  end
+end
+
+class MyGuiState < Fidgit::GuiState
+  def initialize
+    super
+
+    pack :vertical do # Create a vertically packed section.
+      my_label = label "Hello world!", background_color: Gosu::Color.rgb(0, 100, 0)
+      
+      button text: "Goodbye" do
+        subscribe :clicked_left_mouse_button do # Handle mouse-click events.
+          my_label.text = "Goodbye cruel world!"
+        end        
+      end
+    end
+  end
+end
+
+MyGame.new.show
