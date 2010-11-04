0612cfcd3ceda58eca9cee6ad9d3e778f1630bc4
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-11-04 16:25:58 +0000

    Changed composite to be a packer, not a container.

--- lib/fidgit/elements/composite.rb
+++ lib/fidgit/elements/composite.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
-require_relative 'container'
+require_relative 'packer'
 
 module Fidgit
   # A composite element, made up of other elements (but manages them internally).
-  class Composite < Container
+  class Composite < Packer
     DEBUG_BORDER_COLOR = Gosu::Color.rgba(0, 255, 0, 100) # Color to draw an outline in when debugging layout.
 
     # @param (see Element#initialize)
