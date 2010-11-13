0c6d60e795f5d8ff0fa3d665d0ddf36dd5db675f
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-11-13 16:48:15 +0000

    Added Color#+, Color#-, and Color#opaque? (as well as Color.hsva and Color.ahsv)

--- lib/fidgit/gosu_ext/color.rb
+++ lib/fidgit/gosu_ext/color.rb
@@ -2,7 +2,10 @@
 
 module Gosu
   class Color
+    # Is the color completely transparent?
     def transparent?; alpha == 0; end
+    # Is the color completely opaque?
+    def opaque?; alpha == 255; end
 
     # RGB in 0..255 format (Alpha assumed 255)
     #
@@ -15,6 +18,7 @@ module Gosu
     end
 
     # RGBA in 0..255 format
+    #
     # @param [Integer] red
     # @param [Integer] green
     # @param [Integer] blue
@@ -33,6 +37,31 @@ module Gosu
       new(alpha, red, green, blue)
     end
 
+    # HSV format (alpha assumed to be 255)
+    #
+    # @param [Float] hue 0.0..360.0
+    # @param [Float] saturation 0.0..1.0
+    # @param [Float] value 0.0..1.0
+    # @return [Color]
+    def self.hsv(hue, saturation, value)
+      from_hsv(hue, saturation, value)
+    end
+
+    # HSVA format
+    #
+    # @param [Float] hue 0.0..360.0
+    # @param [Float] saturation 0.0..1.0
+    # @param [Float] value 0.0..1.0
+    # @param [Integer] alpha 1..255
+    # @return [Color]
+    def self.hsva(hue, saturation, value, alpha)
+      from_ahsv(alpha, hue, saturation, value)
+    end
+
+    class << self
+      alias_method :ahsv, :from_ahsv
+    end
+
     # Convert from an RGBA array, as used by TexPlay.
     #
     # @param [Array<Float>] color TexPlay color [r, g, b, a] in range 0.0..1.0
@@ -48,9 +77,39 @@ module Gosu
       [red / 255.0, green / 255.0, blue / 255.0,  alpha / 255.0]
     end
 
-    # Show the Color as <RGBA [0, 0, 0, 0]> (default is '(ARGB:0/0/0/0)')
+    # Show the Color as <RGBA [0, 0, 0, 0]> or, if opaque, <RGB [0, 0, 0]> (Gosu default is '(ARGB:0/0/0/0)')
     def to_s
-      "<RGBA [#{red}, #{green}, #{blue}, #{alpha}]>"
+      if opaque?
+        "<RGB [#{red}, #{green}, #{blue}]>"
+      else
+        "<RGBA [#{red}, #{green}, #{blue}, #{alpha}]>"
+      end
+    end
+
+    def +(other)
+      raise ArgumentError, "Can only add another #{self.class}" unless other.is_a? Color
+
+      copy = Color.new(0)
+
+      copy.red = [red + other.red, 255].min
+      copy.green = [green + other.green, 255].min
+      copy.blue = [blue + other.blue, 255].min
+      copy.alpha = [alpha + other.alpha, 255].min
+
+      copy
+    end
+
+    def -(other)
+      raise ArgumentError, "Can only take away another #{self.class}" unless other.is_a? Color
+
+      copy = Color.new(0)
+
+      copy.red = [red - other.red, 0].max
+      copy.green = [green - other.green, 0].max
+      copy.blue = [blue - other.blue, 0].max
+      copy.alpha = [alpha - other.alpha, 0].max
+
+      copy
     end
 
     def ==(other)
--- spec/fidgit/gosu_ext/color_spec.rb
+++ spec/fidgit/gosu_ext/color_spec.rb
@@ -33,6 +33,26 @@ describe Color do
     end
   end
 
+  describe "ahsv" do
+    it "should create a color with the correct values from channel values" do
+      color = Color.ahsv(1, 180.0, 0.5, 0.7)
+      color.hue.should be_within(0.001).of(180.0)
+      color.saturation.should be_within(0.01).of(0.5)
+      color.value.should be_within(0.01).of(0.7)
+      color.alpha.should equal 1
+    end
+  end
+
+  describe "hsva" do
+    it "should create a color with the correct values from channel values" do
+      color = Color.hsva(180.0, 0.5, 0.7, 4)
+      color.hue.should be_within(0.001).of(180.0)
+      color.saturation.should be_within(0.01).of(0.5)
+      color.value.should be_within(0.01).of(0.7)
+      color.alpha.should equal 4
+    end
+  end
+
   describe "from_tex_play" do
     it "should create a color with the correct values" do
       color = Color.from_tex_play([1 / 255.0, 2 / 255.0, 3 / 255.0, 4 / 255.0])
@@ -65,8 +85,46 @@ describe Color do
       Color.rgba(1, 1, 1, 0).should be_transparent
     end
 
-    it "should be fa;se if alpha is > 0" do
+    it "should be false if 0 > alpha > 255" do
       Color.rgba(1, 1, 1, 2).should_not be_transparent
+    end
+
+    it "should be false if alpha == 255" do
+      Color.rgba(1, 1, 1, 255).should_not be_transparent
+    end
+  end
+
+  describe "#opaque?" do
+    it "should be true if alpha is 0" do
+      Color.rgba(1, 1, 1, 0).should_not be_opaque
+    end
+
+    it "should be false if 0 > alpha > 255" do
+      Color.rgba(1, 1, 1, 2).should_not be_opaque
+    end
+
+    it "should be false if alpha == 255" do
+      Color.rgba(1, 1, 1, 255).should be_opaque
+    end
+  end
+
+  describe "#+" do
+    it "should add two colours together" do
+      (Color.rgba(1, 2, 3, 4) + Color.rgba(10, 20, 30, 40)).should == Color.rgba(11, 22, 33, 44)
+    end
+
+    it "should cap values at 255" do
+      (Color.rgba(56, 56, 56, 56) + Color.rgba(200, 200, 200, 200)).should == Color.rgba(255, 255, 255, 255)
+    end
+  end
+
+  describe "#-" do
+    it "should subtract one color from another two colours together" do
+      (Color.rgba(10, 20, 30, 40) - Color.rgba(1, 2, 3, 4)).should == Color.rgba(9, 18, 27, 36)
+    end
+
+    it "should cap values at 0" do
+      (Color.rgba(56, 56, 56, 56) - Color.rgba(57, 57, 57, 57)).should == Color.rgba(0, 0, 0, 0)
     end
   end
 end
